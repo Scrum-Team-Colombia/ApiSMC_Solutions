87b79e86e2d5f73539c55e9d9410a8868b10d075
DanielGonzalezGarzon <111166384+DanielGonzalezGarzon@users.noreply.github.com>
2022-08-25 22:45:11 -0500

    Cambio  clase "Empleados" por "Empleado"
    Se modifico el nombre de la clase con sus métodos constructores "Empleados" por "Empleado"

---
+++ Empleado.java
@@ -0,0 +1,58 @@
+
+package aplicación;
+
+public abstract class Empleado {
+   
+    private String name;
+    private String email;
+    private String enterprise;
+    private String role; 
+
+    //Constructor//
+    public Empleado(String name, String email, String enterprise, String role) {
+        this.name = name;
+        this.email = email;
+        this.enterprise = enterprise;
+        this.role = role;
+    }
+    
+    public Empleado (){
+    
+    }
+
+    //Getter and Setter (Encapsulado)//
+    
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEnterprise() {
+        return enterprise;
+    }
+
+    public void setEnterprise(String enterprise) {
+        this.enterprise = enterprise;
+    }
+    
+    
+}
