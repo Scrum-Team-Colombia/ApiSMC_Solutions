4daca2dfd7540ab8cbdd5b56f8f28d0a87f0ba56
Jose Joaquin Jimenez <joaquinji3@hotmail.com>
2022-09-09 23:53:14 -0500

    //Se realizan cambios

--- src/main/java/com/smcsolutions/smcsolutions/repositorios/EmpresaRepositorio.java
+++ src/main/java/com/smcsolutions/smcsolutions/repositorios/EmpresaRepositorio.java
@@ -1,12 +1,12 @@
 package com.smcsolutions.smcsolutions.repositorios;
 
 
-import com.smcsolutions.smcsolutions.modelo.MovimientoDinero;
+import com.smcsolutions.smcsolutions.modelo.Empresa;
 import java.util.ArrayList;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-public interface empresaRepositorio extends CrudRepository<Empresa, Long> {
+public interface EmpresaRepositorio extends CrudRepository<Empresa, Long> {
     public abstract ArrayList<Empresa> findAllByEmpresa(String empresa);{
     
 }
