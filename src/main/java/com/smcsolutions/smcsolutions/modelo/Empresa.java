6454f6f4e949b66ad3201979a9d2bba7f2bb9476
Juan Carlos Quiroz Prada <111166756+JuanCarlosQuirozPrada311278@users.noreply.github.com>
2022-09-15 00:48:32 -0500

    se realiza cambio en la clase Empresa

--- src/main/java/com/smcsolutions/smcsolutions/modelo/Empresa.java
+++ src/main/java/com/smcsolutions/smcsolutions/modelo/Empresa.java
@@ -1,71 +1,64 @@
-package scrumteamcolombia.smcsolutions.models;
-
-import java.util.ArrayList;
-import java.util.Optional;
+package com.smcsolutions.smcsolutions.modelo;
 
 import javax.persistence.*;
 
 @Entity
-@Table(name = "empresa")
+@Table(name = "Empresa")
 public class Empresa {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(unique = true, nullable = false)
-    private Long id;
-
+    @Column(unique=true, nullable=false)
+    /*private Long id;*/
     private String nombre;
     private String direccion;
-    private String telefono;
+    private int telefono;
     private String nit;
 
+    public Empresa(String nombre, String direccion, int telefono, String nit) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.nit = nit;
+    }
 
-    public Long getId() {
-        return id;
+    public Empresa (){    
     }
-    public void setId(Long id) {
-        this.id = id;
-    }
+
+    
+
     public String getNombre() {
         return nombre;
     }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
     public String getDireccion() {
         return direccion;
     }
+
     public void setDireccion(String direccion) {
         this.direccion = direccion;
     }
-    public String getTelefono() {
+
+    public int getTelefono() {
         return telefono;
     }
-    public void setTelefono(String telefono) {
+
+    public void setTelefono(int telefono) {
         this.telefono = telefono;
     }
+
     public String getNit() {
         return nit;
     }
+
     public void setNit(String nit) {
         this.nit = nit;
     }
-    public ArrayList<Empresa> obtenerEmpresas() {
-        return null;
-    }
-    public Empresa guardarEmpresa(Empresa empresa) {
-        return null;
-    }
-    public Optional<Empresa> obtenerPorId(Integer prioridad) {
-        return null;
-    }
-    public Optional<Empresa> obtenerPorId(Long id2) {
-        return null;
-    }
-    public boolean eliminarEmpresa(Long id2) {
-        return false;
-    }
 
-    
-    
+    public Long getId() {
+        return null;
+    }
 }
